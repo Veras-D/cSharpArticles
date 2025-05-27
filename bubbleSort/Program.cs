3d836eef293732ae4e29c4662eaa528e34ab5a44
Veras-D <dveras2310@gmail.com>
2025-05-26 22:28:21 -0300

    feat(main): Add Program.cs

--- bubbleSort/Program.cs
+++ bubbleSort/Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] array = { 5, 2, 9, 1, 5, 6 };
+
+        Console.WriteLine("Original array:");
+        PrintArray(array);
+
+        BubbleSortOptimized(array);
+
+        Console.WriteLine("Sorted array:");
+        PrintArray(array);
+    }
+
+    public static void BubbleSort(int[] arr)
+    {
+        int n = arr.Length;
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 0; j < n - 1 - i; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void BubbleSortOptimized(int[] arr)
+    {
+        int n = arr.Length;
+        bool swapped;
+        for (int i = 0; i < n - 1; i++)
+        {
+            swapped = false;
+            for (int j = 0; j < n - 1 - i; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    (arr[j], arr[j + 1]) = (arr[j + 1], arr[j]);
+                    swapped = true;
+                }
+            }
+            if (!swapped) break;
+        }
+    }
+
+    public static void PrintArray(int[] arr)
+    {
+        Console.WriteLine(string.Join(", ", arr));
+    }
+}
